e03b29abdc27d467b56ced9d3aa60ddefe132e00
rexim <reximkut@gmail.com>
2022-04-23 10:17:44 +0700

    Implement platform_draw_text() for SDL2 platform

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -xe
 
 clang -Wall -Wextra -Wswitch-enum -c game.c
-clang -Wall -Wextra -Wswitch-enum -o sdl_main sdl_main.c game.o -lSDL2
+clang -Wall -Wextra -Wswitch-enum -o sdl_main sdl_main.c game.o -lSDL2 -lSDL2_ttf
 
 clang -Os -fno-builtin -Wall -Wextra -Wswitch-enum --target=wasm32 --no-standard-libraries -Wl,--export=game_init -Wl,--export=game_render -Wl,--export=game_update -Wl,--export=game_info -Wl,--export=game_keydown -Wl,--no-entry -Wl,--allow-undefined  -o game.wasm game.c
--- sdl_main.c
+++ sdl_main.c
@@ -3,15 +3,18 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 #include "game.h"
 
+#define STB_DS_IMPLEMENTATION
+#include "./stb_ds.h"
+
 #define FACTOR 100
 #define WIDTH (16*FACTOR)
-#define HEIGHT (8*FACTOR)
+#define HEIGHT (9*FACTOR)
 
-void game_update(float dt);
-void game_render(void);
+#define ANEK_LATIN_FILE_PATH "fonts/AnekLatin-Light.ttf"
 
 void scc(int code)
 {
@@ -33,19 +36,80 @@ void *scp(void *ptr)
 static SDL_Renderer *renderer = NULL;
 static SDL_Window *window = NULL;
 
+typedef struct {
+    char *key; // text
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+} Font_Text_Surface;
+
+typedef struct {
+    int key; // ptsize
+    TTF_Font *font;
+    Font_Text_Surface *texts;
+} Font_Cache;
+
+static Font_Cache *font_cache = NULL;
+
 void platform_draw_text(i32 x, i32 y, const char *text, u32 size, u32 color, Align align)
 {
-    (void) x;
-    (void) y;
-    (void) text;
-    (void) size;
+    // TODO: switch to pure FreeType2 for better subpixel anti-aliasing
+    ptrdiff_t font_index = hmgeti(font_cache, (int) size);
+    if (font_index < 0) {
+        Font_Cache item = {0};
+        item.key = size;
+        item.font = scp(TTF_OpenFont(ANEK_LATIN_FILE_PATH, size));
+        TTF_SetFontHinting(item.font, TTF_HINTING_LIGHT);
+        hmputs(font_cache, item);
+        font_index = hmgeti(font_cache, (int) size);
+        assert(font_index >= 0);
+        printf("[LOG] new font size %d\n", (int) size);
+    }
+
+    ptrdiff_t text_index = shgeti(font_cache[font_index].texts, text);
+    if (text_index < 0) {
+        SDL_Color fg = { .r = 0xFF, .g = 0xFF, .b = 0xFF, .a = 0xFF, };
+        Font_Text_Surface item = {0};
+        item.key = (char*) text;
+        item.surface = scp(TTF_RenderText_Solid(font_cache[font_index].font, text, fg));
+        item.texture = SDL_CreateTextureFromSurface(renderer, item.surface);
+        scc(SDL_SetTextureBlendMode(item.texture, SDL_BLENDMODE_BLEND));
+        shputs(font_cache[font_index].texts, item);
+        text_index = shgeti(font_cache[font_index].texts, text);
+        assert(text_index >= 0);
+        printf("[LOG] new text \"%s\"\n", text);
+    }
+
+    SDL_Surface *surface = font_cache[font_index].texts[text_index].surface;
+    SDL_Texture *texture = font_cache[font_index].texts[text_index].texture;
+
+    // TODO: positioning discrepancy between SDL2 and WASM platforms
+    switch (align) {
+        case ALIGN_LEFT: {
+            SDL_Rect dst = { .x = x, .y = y, .w = surface->w, .h = surface->h, };
+            SDL_Rect src = { .x = 0, .y = 0, .w = surface->w, .h = surface->h, };
+            scc(SDL_RenderCopy(renderer, texture, &src, &dst));
+        } break;
+
+        case ALIGN_RIGHT: {
+            assert(0 && "TODO: align right for platform_draw_text()");
+        } break;
+
+        case ALIGN_CENTER: {
+            SDL_Rect dst = { .x = x - surface->w/2, .y = y - surface->h/2, .w = surface->w, .h = surface->h, };
+            SDL_Rect src = { .x = 0, .y = 0, .w = surface->w, .h = surface->h, };
+            scc(SDL_RenderCopy(renderer, texture, &src, &dst));
+        } break;
+
+        default: {
+            assert(0 && "UNREACHABLE");
+        }
+    }
+
+    // TODO: custom color for SDL2 platform_draw_text
     (void) color;
-    (void) align;
-    // TODO: platform_draw_text for SDL2 platform
 }
 
-void platform_fill_rect(int x, int y, int w, int h, uint32_t color)
-{
+void platform_fill_rect(int x, int y, int w, int h, uint32_t color) {
     assert(renderer != NULL);
     SDL_Rect rect = {.x = x, .y = y, .w = w, .h = h,};
     uint8_t r = (color>>(8*0))&0xFF;
@@ -72,6 +136,7 @@ int main()
     game_init(WIDTH, HEIGHT);
 
     scc(SDL_Init(SDL_INIT_VIDEO));
+    scc(TTF_Init());
     window = scp(SDL_CreateWindow(
                      "Snake Native SDL",
                      0, 0,
@@ -111,6 +176,7 @@ int main()
         SDL_Delay(1000/60);
     }
 
+    TTF_Quit();
     SDL_Quit();
 
     return 0;
